2150283325a8e455bdbf2523e1d0464cf523463a
lucasbarbosaalves <lkab05@hotmail.com>
2023-05-30 15:25:36 -0300

    CRUD operations User

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -24,4 +24,27 @@ public class UserService {
 		return obj.get(); // reporta o objeto do tipo User declarado dentro do Optional
 	}
 
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	
+	
+	
 }
